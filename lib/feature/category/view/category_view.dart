db8a0cf0b8d6abb74f9f47b38d37f83b5faf75f7
Burak Şeker <xburakseker@gmail.com>
2023-03-01 17:12:59 +0300

    remove item file

--- lib/feature/category/view/category_view.dart
+++ lib/feature/category/view/category_view.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: must_be_immutable
+
 import 'package:fakestore/feature/category/viewModel/category_view_model.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_mobx/flutter_mobx.dart';
